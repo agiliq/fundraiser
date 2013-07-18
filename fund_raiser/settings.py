ed7947e168f0b829512eee51133b9cf70d843e2b
Akshar Raaj <akshar@agiliq.com>
2013-07-18 23:58:30 +0530

    Move database settings to localsettings

--- fund_raiser/settings.py
+++ fund_raiser/settings.py
@@ -10,18 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'fundraiser',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'agiliq',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -153,3 +141,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from localsettings import *
+except ImportError:
+    pass

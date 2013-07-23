d40045eabb2f05dd2ab8e387467732bcd217ee30
shivakrishnab <shiva@agiliq.com>
2013-07-23 17:44:43 +0530

    Tried django crispy forms

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,10 +4,22 @@ from books.models import Beneficiary, Donor
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+# from django.core.urlresolvers import reverse
+# from crispy_forms.helper import FormHelper
+# from crispy_forms.layout import Submit, Layout, Fieldset
 
 class LoginForm(newform.Form):
     username = newform.CharField()
     password = newform.CharField(widget=newform.PasswordInput)
+
+    # def __init__(self, *args, **kwargs):
+    #     super(LoginForm, self).__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.form_id = 'login-form'
+    #     self.helper.form_class = 'form-horizontal'
+    #     self.helper.form_method = 'post'
+    #     self.helper.form_action = reverse('accounts:login')
+    #     self.helper.add_input(Submit('submit', 'Login', css_class='btn btn-primary'))
 
 
 class BeneficiaryForm(ModelForm):

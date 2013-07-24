48602127a0d8bea4fedf1a1ab83d10f5c60be83e
shivakrishnab <shiva@agiliq.com>
2013-07-24 15:17:28 +0530

    Added campaigns app

---
+++ campaigns/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from campaigns.models import Campaign
+
+
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('beneficiary', 'campaign_name',
+                    'date_created', 'modified', 'target_amount')
+    list_filter = ['beneficiary', 'campaign_name']
+    search_fields = ['beneficiary', 'campaign_name']
+
+admin.site.register(Campaign, CampaignAdmin)

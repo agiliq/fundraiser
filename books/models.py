da223c38f03f007c27eac4dd96136ec472324ddb
shivakrishnab <shiva@agiliq.com>
2013-07-24 11:58:18 +0530

    Modified models.py and moved Beneficiary and Donor to stakeholder models.py app

--- books/models.py
+++ books/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Publisher(models.Model):
@@ -18,31 +17,9 @@ class Book(models.Model):
     image = models.ImageField(upload_to="book_covers/")
     title = models.CharField(max_length=100)
     synopsis = models.TextField(blank=True)
-    cost = models.DecimalField(blank=True, max_digits=10, decimal_places=2, default=0.00)
+    cost = models.DecimalField(
+        blank=True, max_digits=10, decimal_places=2, default=0.00)
     author = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.title
-
-
-class Beneficiary(models.Model):
-    user = models.OneToOneField(User)
-    address = models.CharField(max_length=150)
-    website = models.URLField(blank=True, null=True)
-    is_approved = models.BooleanField(blank=True, default=False)
-
-    class Meta:
-        verbose_name = "Beneficiary"
-        verbose_name_plural = "Beneficiaries"
-
-    def __unicode__(self):
-        return self.user.username
-
-
-class Donor(models.Model):
-    user = models.OneToOneField(User)
-    address = models.CharField(max_length=150)
-    website = models.URLField(blank=True, null=True)
-
-    def __unicode__(self):
-        return "{0}".format(self.user.username)

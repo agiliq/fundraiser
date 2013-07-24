a05b2f26fee629165244c6d2ef0d8ef2b2641a24
shivakrishnab <shiva@agiliq.com>
2013-07-24 15:11:09 +0530

    Deleted the unwanted urls and moved to other apps

--- fund_raiser/urls.py
+++ fund_raiser/urls.py
@@ -1,38 +1,32 @@
 from django.conf.urls import patterns, include, url
-from fund_raiser.views import pbadmin_index, approve, create_a_campaign
+from authentication.views import customadmin_index, approve
 from django.contrib import admin
-from django.views.generic import ListView
 from django.contrib.auth.models import User
-from books.models import Donor, Beneficiary
-from fund_raiser.models import Campaign
+from django.views.generic import ListView
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^customadmin/$', customadmin_index, name='customeadmin_index'),
+                       url(r'^customadmin/unapproved-users$', ListView.as_view(
+                           template_name='authentication/unapproved_users.html',
+                           queryset=User.objects.filter(
+                           beneficiary__is_approved=False),
+                           context_object_name="unapproved_users"), name='unapproved'),
+                       url(r'^customadmin/approve/(?P<user_id>\d+)$',
+                           approve, name='approve'),
+
+
                        url(r'^accounts/', include(
                            'authentication.urls', namespace='accounts')),
+
+
                        url(r'^books/', include(
                            'books.urls', namespace='books')),
-                       url(r'^pbadmin/$', pbadmin_index, name='pbadmin_index'),
-                       url(r'^pbadmin/unapproved-users$', ListView.as_view(
-                           template_name='unapproved_users.html',
-                           queryset=User.objects.filter(
-                           beneficiary__approved=False),
-                           context_object_name="unapproved_users"), name='unapproved'),
-                       url(r'^pbadmin/approve/(?P<user_id>\d+)$',
-                           approve, name='approve'),
-                       url(r'^donors/$', ListView.as_view(
-                           template_name='donors.html',
-                           queryset=Donor.objects.all(),
-                           context_object_name='donor_list'), name='list_of_donors'),
-                       url(r'^beneficiaries/$', ListView.as_view(
-                           template_name='beneficiaries.html',
-                           queryset=Beneficiary.objects.all(),
-                           context_object_name='beneficiary_list'), name='list_of_bnfs'),
-                       url(r'^campaigns/$', ListView.as_view(
-                           template_name='campaigns.html',
-                           queryset=Campaign.objects.all(),
-                           context_object_name='campaign_list'), name='list_of_campaigns'),
-                       url(r'^create_a_campaign/$',
-                           create_a_campaign, name='create_a_campaign'),
+
+                       url(r'^people/', include(
+                           'people.urls', namespace='people')),
+
+                       url(r'^campaigns/', include(
+                           'campaigns.urls', namespace='campaigns')),
                        )

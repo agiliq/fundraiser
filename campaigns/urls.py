4eb7e902ff7668916becacc583abcf45fdec38d2
shivakrishnab <shiva@agiliq.com>
2013-07-25 20:06:59 +0530

    Modified campaigns/urls.py

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -6,7 +6,7 @@ from campaigns.views import create_a_campaign
 
 urlpatterns = patterns('',
 url(r'^campaigns/$', ListView.as_view(
-                           template_name='campaigns.html',
+                           template_name='campaigns/campaigns.html',
                            queryset=Campaign.objects.all(),
                            context_object_name='campaign_list'), name='list_of_campaigns'),
                        url(r'^create_a_campaign/$',

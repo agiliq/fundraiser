6b1003bce2e2a9508596b2e7b61e4a375b61e2a7
shivakrishnab <shiva@agiliq.com>
2013-07-25 18:48:29 +0530

    Moved custom admin related links to authentication urls file

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from authentication.views import register, user_login, user_logout
 from django.views.generic import TemplateView
 
+from authentication.views import customadmin_index, approve, UnapprovedUsers
 
 urlpatterns = patterns('',
                        url(r'^register/donor$', register, name='donor'),
@@ -12,4 +13,7 @@ urlpatterns = patterns('',
                        url(r'^login/$', user_login, name='login'),
                        url(r'^logout/$', user_logout, name='logout'),
 
+                       url(r'^$', customadmin_index, name='customadmin_index'),
+                       url(r'^unapproved-users$', UnapprovedUsers, name='unapproved'),
+                       url(r'^approve/(?P<user_id>\d+)$', approve, name='approve'),
                        )

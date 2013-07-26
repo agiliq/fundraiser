10695169d47c10ca7f02205ddc0562bb938b7f4f
Akshar Raaj <akshar@agiliq.com>
2013-07-26 16:19:12 +0530

    Use class based view for registration. Do not need separate registration form for Donor and Beneficiary

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(UserCreationForm):
+    address = forms.CharField()
+    website = forms.URLField()
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url
 
-from authentication.views import register, user_login, user_logout
+from authentication.views import user_login, user_logout
 from authentication.views import customadmin_index, approve, UnapprovedUsers
+from authentication.views import BeneficiaryRegistrationView, DonorRegistrationView
 
 
 urlpatterns = patterns('',
-   url(r'^register/donor$', register, name='donor'),
+   url(r'^register/donor$', DonorRegistrationView.as_view(), name='donor'),
    url(r'^register/beneficiary$',
-       register, name='beneficiary'),
+       BeneficiaryRegistrationView.as_view(), name='beneficiary'),
    url(r'^login/$', user_login, name='login'),
    url(r'^logout/$', user_logout, name='logout'),
 

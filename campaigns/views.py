b4db6e7374b8b3b6f91b3e60481bbc47a8871757
shivakrishnab <shiva@agiliq.com>
2013-07-26 15:15:37 +0530

    Added campaign Detail classed based view in campaigns/views.py

--- campaigns/views.py
+++ campaigns/views.py
@@ -3,10 +3,11 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from people.models import Beneficiary
 from campaigns.models import Campaign
 from campaigns.forms import CampaignForm
-
 
 
 @login_required
@@ -16,16 +17,27 @@ def create_a_campaign(request):
         if request.method == 'POST':
             form = CampaignForm(request.POST)
             if form.is_valid():
-                beneficiary =  Beneficiary.objects.get(user=request.user)
+                beneficiary = Beneficiary.objects.get(user=request.user)
                 cam_obj = Campaign.objects.create(beneficiary=beneficiary,
-                                         campaign_name=request.POST['campaign_name'],
-                                         target_amount=request.POST['target_amount'],
-                                         cause=request.POST['cause'])
+                                                  campaign_name=request.POST['campaign_name'],
+                                                  target_amount=request.POST['target_amount'],
+                                                  cause=request.POST['cause'])
                 if request.POST.getlist('books'):
                     for m2m in request.POST.getlist('books'):
                         cam_obj.books.add(m2m)
                 return HttpResponseRedirect(reverse('campaigns:list_of_campaigns'))
 
-        return render_to_response('campaigns/create_a_campaign.html', {'form':form, 'errors':dict(form.errors.viewitems())}, context_instance=RequestContext(request))
+        return render_to_response('campaigns/create_a_campaign.html', {'form': form, 'errors': dict(form.errors.viewitems())}, context_instance=RequestContext(request))
     else:
         return render_to_response('campaigns/campaign_unapproved.html', context_instance=RequestContext(request))
+
+
+def campaign_detail(request, campaign_id):
+    print campaign_id,">>>>>>>>>>>>>>>>>>>>>>>>>>"
+    campaign = Campaign.objects.get(id=int(campaign_id))
+    return render_to_response("campaigns/campaign_detail.html", {'campaign':campaign}, context_instance=RequestContext(request))
+
+class CampaignDetail(generic.DetailView):
+    model = Campaign
+    template_name = 'campaigns/campaign_detail.html'
+    context_object_name = 'campaign'

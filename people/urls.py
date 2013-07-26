8c850bc7b7a6810e24ea41903ad4c7ac7b6d7a6c
shivakrishnab <shiva@agiliq.com>
2013-07-26 13:01:48 +0530

    Adjusted url alignment

--- people/urls.py
+++ people/urls.py
@@ -4,12 +4,8 @@ from people.models import Donor, Beneficiary
 
 
 urlpatterns = patterns('',
-                       url(r'^beneficiaries/$', ListView.as_view(
-                           template_name='people/beneficiaries.html',
-                           queryset=Beneficiary.objects.all(),
-                           context_object_name='beneficiary_list'), name='list_of_bnfs'),
-                       url(r'^donors/$', ListView.as_view(
-                           template_name='people/donors.html',
-                           queryset=Donor.objects.all(),
-                           context_object_name='donor_list'), name='list_of_donors'),
-                       )
+    url(r'^beneficiaries/$', ListView.as_view(template_name='people/beneficiaries.html',
+                queryset=Beneficiary.objects.all(),context_object_name='beneficiary_list'), name='list_of_bnfs'),
+    url(r'^donors/$', ListView.as_view(template_name='people/donors.html',
+                queryset=Donor.objects.all(),context_object_name='donor_list'), name='list_of_donors'),
+    )

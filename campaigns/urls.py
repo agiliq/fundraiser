82f3957fd563791b876d23a36fe5079494e6e8ae
shivakrishnab <shiva@agiliq.com>
2013-07-26 15:14:51 +0530

    Added campaign Detail view in campaigns/urls.py

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView
+
 from campaigns.models import Campaign
-from campaigns.views import create_a_campaign
+from campaigns.views import create_a_campaign, CampaignDetail
 
 
 urlpatterns = patterns('',
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
                            queryset=Campaign.objects.all(),
                            context_object_name='campaign_list'), name='list_of_campaigns'),
     url(r'^create_a_campaign/$',create_a_campaign, name='create_a_campaign'),
+    url(r'^campaign_detail/(?P<pk>\d+)/$',CampaignDetail.as_view(), name='campaign_detail'),
     )

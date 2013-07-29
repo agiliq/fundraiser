b1f268afbbbacaae73b41f9af2f9fc08e1998fcc
shivakrishnab <shiva@agiliq.com>
2013-07-29 16:34:53 +0530

    Added pagination to list of donors, beneficiaries

--- people/urls.py
+++ people/urls.py
@@ -5,7 +5,7 @@ from people.models import Donor, Beneficiary
 
 urlpatterns = patterns('',
     url(r'^beneficiaries/$', ListView.as_view(template_name='people/beneficiaries.html',
-                queryset=Beneficiary.objects.all(),context_object_name='beneficiary_list'), name='list_of_bnfs'),
+                queryset=Beneficiary.objects.all(), context_object_name='beneficiary_list', paginate_by=10), name='list_of_bnfs'),
     url(r'^donors/$', ListView.as_view(template_name='people/donors.html',
-                queryset=Donor.objects.all(),context_object_name='donor_list'), name='list_of_donors'),
+                queryset=Donor.objects.all(), context_object_name='donor_list', paginate_by=10), name='list_of_donors'),
     )

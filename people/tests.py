62e1d55580145c6bef4a7e55a1adfa51ba3c61bc
shivakrishnab <shiva@agiliq.com>
2013-07-29 12:09:35 +0530

    Added test cases for people app

--- people/tests.py
+++ people/tests.py
@@ -8,9 +8,28 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+# from books.models import Book, Publisher
+
+class PeopleAppTestcase(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user(username="admin",email="admin@agiliq.com",password="admin")
+
+    def test_BeneficiariesListView(self):
+        response = self.c.get(reverse("people:list_of_bnfs"))
+        self.assertEqual(200, response.status_code)
+        self.c.login(username="admin", password="admin")
+        response = self.c.get(reverse("people:list_of_bnfs"))
+        self.assertEqual(200, response.status_code)
+
+    def test_DonorsListView(self):
+        response = self.c.get(reverse("people:list_of_donors"))
+        self.assertEqual(200, response.status_code)
+        self.c.login(username="admin", password="admin")
+        response = self.c.get(reverse("people:list_of_donors"))
+        self.assertEqual(200, response.status_code)

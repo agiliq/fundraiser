58e0641cc6b2aae6f23bf9708ca945070f07d36e
shivakrishnab <shiva@agiliq.com>
2013-07-29 14:00:29 +0530

    Added campaign edit/update url in campaigns/urls.py

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from campaigns.views import create_a_campaign, CampaignDetail, CampaignsListView
+from campaigns.views import create_a_campaign, CampaignDetail, CampaignsListView, CampaignUpdate
 
 
 urlpatterns = patterns('',
     url(r'^$', CampaignsListView.as_view(), name='list_of_campaigns'),
     url(r'^create_a_campaign/$',create_a_campaign, name='create_a_campaign'),
     url(r'^campaign_detail/(?P<pk>\d+)/$',CampaignDetail.as_view(), name='campaign_detail'),
+    url(r'^campaign/edit/(?P<pk>\d+)/$',CampaignUpdate.as_view(), name='edit_campaign'),    
     )

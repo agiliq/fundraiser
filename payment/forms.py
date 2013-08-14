5a369ae53c0558444699a48d88f390c0913fabe3
shivakrishnab <shiva@agiliq.com>
2013-08-14 19:58:46 +0530

    Changed ebspayment app name to payment and modified html file code and filenames

---
+++ payment/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from django.conf import settings
+
+from .fields import CountryField
+from billing import CreditCard
+
+import datetime
+# Transaction Modes
+MODES = (
+    ('TEST', 'TEST'),
+    ('LIVE', 'LIVE'),
+)
+
+# EBS Payment Form
+
+
+class PaymentForm(forms.Form):
+
+    account_id = forms.IntegerField(
+        initial=settings.EBS_ACCOUNT_ID, required=True)
+    reference_no = forms.IntegerField(required=True, initial="223")
+    amount = forms.DecimalField(
+        max_digits=10, decimal_places=2, required=True, initial='1.00')
+    name = forms.CharField(max_length=255, required=True, initial="Sheeba")
+    email = forms.EmailField(
+        max_length=70, required=True, initial="xyz@ebs.in")
+    address = forms.CharField(required=True, initial="Kodambakkam")
+    city = forms.CharField(max_length=60, required=True, initial="Chennai")
+    state = forms.CharField(max_length=60, required=True, initial="Tamil Nadu")
+    country = CountryField()
+    postal_code = forms.IntegerField(required=True, initial="600098")
+    phone = forms.IntegerField(required=True, initial="044123456")
+    # ship_name = forms.CharField(50, label="Shipping Name",initial="Sheeba V")
+    # ship_address = forms.CharField(100, label="Shipping Address", initial="Saketh")
+    # ship_city = forms.CharField(40, label="Shipping City", initial="New Delhi")
+    # ship_state = forms.CharField(40, label="Shipping State", initial="Delhi")
+    # ship_country = CountryField(label="Shipping Country")
+    # ship_postal_code = forms.CharField(20, label="Shipping Postal Code", initial="110098")
+    # ship_phone = forms.CharField(20, label="Shipping Phone", initial="011123456")
+    return_url = forms.URLField(
+        label="Return URL", required=True, initial=settings.EBS_RETURN_URL + '?DR={DR}')
+    description = forms.CharField(required=True, initial="Testing EBS-Django")
+    mode = forms.ChoiceField(choices=MODES)
+
+# Stripe payment form
+
+CARD_TYPES = [
+    ('', ''),
+    ('visa', 'Visa'),
+    ('master', 'Master'),
+    ('discover', 'Discover'),
+    ('american_express', 'American Express'),
+    ('diners_club', 'Diners Club'),
+    # ('jcb', ''),
+    # ('switch', ''),
+    # ('solo', ''),
+    # ('dankort', ''),
+    ('maestro', 'Maestro'),
+    # ('forbrugsforeningen', ''),
+    # ('laser', 'Laser'),
+    ]
+
+today = datetime.date.today()
+MONTH_CHOICES = [(m, datetime.date(today.year, m, 1).strftime('%b')) for m in range(1, 13)]
+YEAR_CHOICES = [(y, y) for y in range(today.year, today.year + 21)]
+
+
+class CreditCardForm(forms.Form):
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    month = forms.ChoiceField(choices=MONTH_CHOICES)
+    year = forms.ChoiceField(choices=YEAR_CHOICES)
+    number = forms.CharField(required=False)
+    card_type = forms.ChoiceField(choices=CARD_TYPES, required=False)
+    verification_value = forms.CharField(label='CVV', required=False)
+
+    def clean(self):
+        data = self.cleaned_data
+        credit_card = CreditCard(**data)
+        if not credit_card.is_valid():
+            raise forms.ValidationError('Credit card validation failed')
+        return data
---
+++ payment/views.py
@@ -0,0 +1,107 @@
+# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.conf import settings
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+
+import base64
+import re
+import hashlib
+
+from campaigns.models import Campaign
+from .forms import PaymentForm
+
+from .forms import CreditCardForm
+from billing import CreditCard, get_gateway
+
+
+# Ebs payment gateway functions ####################
+
+def ebspayment(request, campaign_id):
+    campaign_obj = get_object_or_404(Campaign, pk=campaign_id)
+    # hashstring = hashlib.md5(settings.EBS_ACCOUNT_ID+'|'+settings.EBS_SECRET_KEY).hexdigest()
+    form = PaymentForm()
+
+    # string = settings.EBS_SECRET_KEY+"|"+form.cleaned_data['account_id']+"|"
+    #               +form.cleaned_data['amount']+"|"+form.cleaned_data['reference_no'] +"|"
+    #               +form.cleaned_data['return_url']+"|"+form.cleaned_data['mode']
+    string = settings.EBS_SECRET_KEY + "|" + '5880' + "|" + \
+        '1.00' + "|" + '223' + "|" + settings.EBS_RETURN_URL + "|" + "TEST"
+    print string
+    secure_hash = hashlib.md5(string).hexdigest()
+    return render_to_response('payment/payment_form.html', {'form': form,
+                                                               'campaign': campaign_obj,
+                                                               'ebs_url': settings.EBS_ACTION_URL,
+                                                               'secure_hash': secure_hash},
+                              context_instance=RequestContext(request))
+
+# function to process response from EBS and decrypt
+
+
+def ebsresponse(request):
+    if request.method == 'GET':
+        formvalue = request.GET
+        drvalue = formvalue.get('DR')
+        dr = re.sub('\s', '\+', drvalue)
+        data = base64.b64decode(dr)
+        key = settings.EBS_SECRET_KEY
+        finalvalue = RC4(data, key)
+        params = []
+        params = finalvalue.split('&')
+        paramdetail = {}
+        for param in params:
+            k, v = param.split('=')
+            paramdetail[k] = v
+
+    else:
+        paramdetail = {}
+    return render_to_response('payment/response.html', {'response': paramdetail})
+
+# RC4 Decryption function.Do Not edit it.
+
+
+def RC4(data, key):
+    x = 0
+    s = range(256)
+    for i in range(256):
+        x = (x + s[i] + ord(key[i % len(key)])) % 256
+        s[i], s[x] = s[x], s[i]
+    x = y = 0
+    out = ""
+    for c in data:
+        x = (x + 1) % 256
+        y = (y + s[x]) % 256
+        s[x], s[y] = s[y], s[x]
+        out += chr(ord(c) ^ s[(s[x] + s[y]) % 256])
+    return out
+# Do Not Edit upto this
+
+# Stripe payment gateway functions ####################
+
+
+def stripepayment(request,  campaign_id):
+    campaign_obj = get_object_or_404(Campaign, pk=campaign_id)
+    amount = 1
+    response= None
+    if request.method == 'POST':
+        form = CreditCardForm(request.POST)
+        # import ipdb; ipdb.set_trace()
+        if form.is_valid():
+            data = form.cleaned_data
+            credit_card = CreditCard(**data)
+            merchant = get_gateway("stripe")
+            response = merchant.purchase(amount,credit_card)
+            return HttpResponseRedirect(reverse('paygate:payment_success', kwargs={'response':response}))
+    else:
+        form = CreditCardForm(initial={'number':'4242424242424242'})
+    return render_to_response('payment/stripe_payment_form.html',{'form': form,
+                                             'amount':amount,
+                                             'response':response,
+                                             'campaign': campaign_obj},
+                                             context_instance=RequestContext(request))
+
+
+class PaymentSuccess(TemplateView):
+    template_name = 'payment/payment_success.html'

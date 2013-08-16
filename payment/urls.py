6a31b429b4b51e69e8e0bf19519294875028e0e6
shivakrishnab <shiva@agiliq.com>
2013-08-16 19:39:51 +0530

    Modified payment/urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, patterns
+from django.contrib.auth.decorators import login_required
 
 from .views import ebspayment, ebsresponse, stripepayment, PaymentSuccess
 
 
 urlpatterns = patterns('',
-    url(r'^ebspayment/(?P<campaign_id>\d+)/$', ebspayment, name='ebsindex'),
-    url(r'^ebspayment/response', ebsresponse, name='ebsresponse'),
+    url(r'^ebspayment/(?P<campaign_id>\d+)/$', login_required(ebspayment), name='ebsindex'),
+    url(r'^ebspayment/response', login_required(ebsresponse), name='ebsresponse'),
 
     url(r'^payment/stripe/(?P<campaign_id>\d+)/$', stripepayment, name='stripeindex'),
-    url(r'^payment/success/$', PaymentSuccess.as_view(), name='payment_success'),
+    url(r'^payment/success/(?P<campaign_id>\d+)/$', PaymentSuccess.as_view(), name='payment_success'),
+    # url(r'^payment/success/$', PaymentSuccess, name='payment_success'),
 )

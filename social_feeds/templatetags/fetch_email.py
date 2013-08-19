276a98c12614bc86b177c45d2d72d9ac86f62849
shivakrishnab <shiva@agiliq.com>
2013-08-19 19:34:52 +0530

    Added a template tag that filters email from the string

---
+++ social_feeds/templatetags/fetch_email.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def fetch_email(value):
+	if value:
+		values = value.split(' ')
+		for val in values:
+			if '@' in val:
+				return val

e14572500ff9525e33e8c756d9fb6ddca78f62ce
Shiva <shiva@agiliq.com>
2013-12-11 16:44:33 +0530

    Commented paymentsuccess url, view as they not used

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 from django.contrib.auth.decorators import login_required
 
-from .views import ebspayment, ebsresponse, stripepayment, PaymentSuccess
+from .views import ebspayment, ebsresponse, stripepayment
 
 
 urlpatterns = patterns('',
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     url(r'^ebspayment/response', login_required(ebsresponse), name='ebsresponse'),
 
     url(r'^stripe/(?P<campaign_id>\d+)/$', stripepayment, name='stripeindex'),
-    url(r'^success/(?P<campaign_id>\d+)/$', PaymentSuccess.as_view(), name='payment_success'),
+    # url(r'^success/(?P<campaign_id>\d+)/$', PaymentSuccess.as_view(), name='payment_success'),
     # url(r'^payment/success/$', PaymentSuccess, name='payment_success'),
 )

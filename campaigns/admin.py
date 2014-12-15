dce856348ac2f803affd83991e0c6f94c2dca4ef
Yogesh Kumar <yk.301189@gmail.com>
2014-12-15 16:11:02 +0530

    Updated admin.py

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -15,8 +15,20 @@ class CampaignAdmin(admin.ModelAdmin):
     search_fields = ['person', 'campaign_name']
     prepopulated_fields = {"slug": ("campaign_name",)}
 
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ['name', 'role', 'short_description', 'fb_url', 'campaign']
+
+
+class FundDistributionAdmin(admin.ModelAdmin):
+    list_display = ['usage', 'allocation', 'campaign']
+
+
+class RewardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'campaign']
+
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Category)
-admin.site.register(FundDistribution)
-admin.site.register(Reward)
-admin.site.register(TeamMember)
+admin.site.register(FundDistribution, FundDistributionAdmin)
+admin.site.register(Reward, RewardAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
